ba409cbaff694bf0744496baeb5436fdaebc1d30
kazukazupi <22kazuaki22@gmail.com>
2025-02-11 20:40:42 +0900

    remember opponent last obs

--- run_sample_opponent_ppo.py
+++ run_sample_opponent_ppo.py
@@ -52,10 +52,11 @@ def main():
     train_csv_paths: Dict[AgentID, str] = {}
     eval_csv_paths: Dict[AgentID, str] = {}
     vec_envs = {}
+    opponents_last_obs = {}
 
     agnet_ids = ["robot_1", "robot_2"]
 
-    for a in agnet_ids:
+    for a, o in zip(agnet_ids, reversed(agnet_ids)):
 
         # Initialize environment
         # TODO: trainingのTrue/Falseをエージェントごとに個別設定
@@ -69,8 +70,6 @@ def main():
             connections_1=connections_1,
             connections_2=connections_2,
         )
-
-        observations = vec_envs[a].reset()
 
         # Create agent
         obs_dim = vec_env.observation_space(a).shape[0]
@@ -105,6 +104,7 @@ def main():
         )
 
         # Create rollouts
+        observations = vec_envs[a].reset()
         rollouts[a] = RolloutStorage(
             num_steps=args.num_steps,
             num_processes=args.num_processes,
@@ -113,6 +113,7 @@ def main():
         )
         rollouts[a].obs[0].copy_(observations[a])
         rollouts[a].to(args.device)
+        opponents_last_obs[o] = observations[o]
 
         # Create log files
         log_dirs[a] = os.path.join(args.save_path, a)
@@ -143,7 +144,7 @@ def main():
 
                 with torch.no_grad():
                     value, actions[a], action_log_prob = agents[a].act(rollouts[a].obs[step])
-                    _, actions[o], _ = opponents[o].act(observations[o], deterministic=True)
+                    _, actions[o], _ = opponents[o].act(opponents_last_obs[o], deterministic=True)
 
                 observations, rewards, dones, infos = vec_envs[a].step(actions)
 
@@ -158,6 +159,7 @@ def main():
                 bad_masks = torch.FloatTensor([[0.0] if info["TimeLimit.truncated"] else [1.0] for info in infos[a]])
 
                 rollouts[a].insert(observations[a], actions[a], action_log_prob, value, rewards[a], masks, bad_masks)
+                opponents_last_obs[o] = observations[o]
 
             with torch.no_grad():
                 next_value = agents[a].get_value(rollouts[a].obs[-1]).detach()

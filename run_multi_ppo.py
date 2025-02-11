4ab916676280c5eb9aacca506b447699e77badce
kazukazupi <22kazuaki22@gmail.com>
2025-02-11 13:42:46 +0900

    implemented multiagent ppo

--- run_multi_ppo.py
+++ run_multi_ppo.py
@@ -1,13 +1,13 @@
 import csv
 import os
-from collections import deque
+from typing import Dict
 
 import numpy as np
 import torch
 from evogym import get_full_connectivity
 
 from alg.ppo import PPO, Agent, RolloutStorage, update_linear_schedule
-from envs import make_vec_envs
+from envs import AgentID, make_vec_envs
 from evaluate import evaluate
 from utils import get_args
 
@@ -28,15 +28,8 @@ def main():
         ]
     )
     connections_1 = get_full_connectivity(body_1)
-    body_2 = np.array(
-        [
-            [0, 0, 3, 3, 3],
-            [0, 0, 2, 2, 2],
-            [0, 0, 2, 2, 2],
-            [0, 0, 2, 2, 2],
-            [0, 0, 0, 0, 0],
-        ]
-    )
+
+    body_2 = np.fliplr(body_1)
     connections_2 = get_full_connectivity(body_2)
 
     vec_env = make_vec_envs(
@@ -50,126 +43,130 @@ def main():
         connections_2=connections_2,
     )
 
-    obs_dim = vec_env.observation_space("robot_1").shape[0]
-    action_dim = vec_env.action_space("robot_1").shape[0]
-
-    agent = Agent(
-        obs_dim=obs_dim,
-        hidden_dim=64,
-        action_dim=action_dim,
-    )
-    agent.to(args.device)
-
-    updater = PPO(
-        agent,
-        args.clip_param,
-        args.ppo_epoch,
-        args.num_mini_batch,
-        args.value_loss_coef,
-        args.entropy_coef,
-        args.lr,
-        args.eps,
-        args.max_grad_norm,
-    )
-
-    rollouts = RolloutStorage(
-        num_steps=args.num_steps,
-        num_processes=args.num_processes,
-        obs_dim=obs_dim,
-        action_dim=action_dim,
-    )
+    agents: Dict[AgentID, Agent] = {}
+    updaters: Dict[AgentID, PPO] = {}
+    rollouts: Dict[AgentID, RolloutStorage] = {}
+    max_determ_avg_rewards: Dict[AgentID, np.ndarray] = {}
+    log_dirs: Dict[AgentID, str] = {}
+    train_csv_paths: Dict[AgentID, str] = {}
+    eval_csv_paths: Dict[AgentID, str] = {}
 
     observations = vec_env.reset()
-    obs = observations["robot_1"]
-    rollouts.obs[0].copy_(obs)
-    rollouts.to(args.device)
 
-    episode_rewards = deque(maxlen=10)
+    for a in vec_env.agents:
 
-    max_determ_avg_reward = float("-inf")
+        # Create agent
+        obs_dim = vec_env.observation_space(a).shape[0]
+        action_dim = vec_env.action_space(a).shape[0]
+        agents[a] = Agent(
+            obs_dim=obs_dim,
+            hidden_dim=64,
+            action_dim=action_dim,
+        )
+        agents[a].to(args.device)
 
-    train_csv_path = os.path.join(args.save_path, "train_log.csv")
-    eval_csv_path = os.path.join(args.save_path, "eval_log.csv")
+        # Create updater
+        updaters[a] = PPO(
+            agents[a],
+            args.clip_param,
+            args.ppo_epoch,
+            args.num_mini_batch,
+            args.value_loss_coef,
+            args.entropy_coef,
+            args.lr,
+            args.eps,
+            args.max_grad_norm,
+        )
 
-    with open(train_csv_path, "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(["updates", "num timesteps", "mean reward", "median reward", "min reward", "max reward"])
+        # Create rollouts
+        rollouts[a] = RolloutStorage(
+            num_steps=args.num_steps,
+            num_processes=args.num_processes,
+            obs_dim=obs_dim,
+            action_dim=action_dim,
+        )
+        rollouts[a].obs[0].copy_(observations[a])
+        rollouts[a].to(args.device)
 
-    with open(eval_csv_path, "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(["updates", "num timesteps", "rewrard", "max reward"])
+        # Create log files
+        log_dirs[a] = os.path.join(args.save_path, a)
+        os.mkdir(log_dirs[a])
+
+        train_csv_paths[a] = os.path.join(log_dirs[a], "train_log.csv")
+        eval_csv_paths[a] = os.path.join(log_dirs[a], "eval_log.csv")
+
+        with open(train_csv_paths[a], "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(["updates", "num timesteps", "train reward"])
+
+        with open(eval_csv_paths[a], "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(["updates", "num timesteps", "eval rewrard"])
+
+        max_determ_avg_rewards[a] = float("-inf")
 
     for j in range(args.num_updates):
 
-        update_linear_schedule(updater.optimizer, j, args.num_updates, args.lr)
+        for a in vec_env.agents:
+            update_linear_schedule(updaters[a].optimizer, j, args.num_updates, args.lr)
 
+        # Sample actions
         for step in range(args.num_steps):
 
+            values = {}
+            actions = {}
+            action_log_probs = {}
+
+            for a in vec_env.agents:
+                with torch.no_grad():
+                    values[a], actions[a], action_log_probs[a] = agents[a].act(rollouts[a].obs[step])
+
+            observations, rewards, dones, infos = vec_env.step(actions)
+
+            for a in vec_env.agents:
+
+                # Store data
+                masks = torch.FloatTensor([[0.0] if done else [1.0] for done in dones[a]])
+                bad_masks = torch.FloatTensor([[0.0] if info["TimeLimit.truncated"] else [1.0] for info in infos[a]])
+                rollouts[a].insert(
+                    observations[a], actions[a], action_log_probs[a], values[a], rewards[a], masks, bad_masks
+                )
+
+                # Log
+                for info in infos[a]:
+                    if "episode" in info:
+                        total_num_steps = (j + 1) * args.num_processes * args.num_steps
+                        with open(train_csv_paths[a], "a") as f:
+                            writer = csv.writer(f)
+                            writer.writerow(
+                                [
+                                    j,
+                                    total_num_steps,
+                                    info["episode"]["r"],
+                                ]
+                            )
+
+        # Update
+        next_values = {}
+
+        for a in vec_env.agents:
+
             with torch.no_grad():
-                value, action, action_log_prob = agent.act(rollouts.obs[step])
+                next_values[a] = agents[a].get_value(rollouts[a].obs[-1]).detach()
 
-            action_2 = torch.Tensor(np.array([vec_env.action_space("robot_2").sample()]))
+            rollouts[a].compute_returns(
+                next_values[a], args.use_gae, args.gamma, args.gae_lambda, args.use_proper_time_limits
+            )
+            updaters[a].update(rollouts[a])
+            rollouts[a].after_update()
 
-            actions = {"robot_1": action, "robot_2": action_2}
+        if j % args.eval_interval == 0:
 
-            observations, rewards_, dones_, infos_ = vec_env.step(actions)
-            obs = observations["robot_1"]
-            rewards = rewards_["robot_1"]
-            dones = dones_["robot_1"]
-            infos = infos_["robot_1"]
-
-            for info in infos:
-                if "episode" in info:
-                    episode_rewards.append(info["episode"]["r"])
-
-            masks = torch.FloatTensor([[0.0] if done else [1.0] for done in dones])
-            bad_masks = torch.FloatTensor([[0.0] if info["TimeLimit.truncated"] else [1.0] for info in infos])
-
-            rollouts.insert(obs, action, action_log_prob, value, rewards, masks, bad_masks)
-
-        with torch.no_grad():
-            next_value = agent.get_value(rollouts.obs[-1]).detach()
-
-        rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.gae_lambda, args.use_proper_time_limits)
-
-        updater.update(rollouts)
-
-        rollouts.after_update()
-
-        if j % args.log_interval == 0 and len(episode_rewards) > 1:
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
 
-            with open(train_csv_path, "a") as f:
-                writer = csv.writer(f)
-                writer.writerow(
-                    [
-                        j,
-                        total_num_steps,
-                        np.mean(episode_rewards),
-                        np.median(episode_rewards),
-                        np.min(episode_rewards),
-                        np.max(episode_rewards),
-                    ]
-                )
-
-            print(
-                "Updates {}, num timesteps {}\nLast {} training episodes:"
-                "mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n".format(
-                    j,
-                    total_num_steps,
-                    len(episode_rewards),
-                    np.mean(episode_rewards),
-                    np.median(episode_rewards),
-                    np.min(episode_rewards),
-                    np.max(episode_rewards),
-                )
-            )
-
-        if j % args.eval_interval == 0 and len(episode_rewards) > 1:
-
             obs_rms_dict = vec_env.obs_rms_dict
-            results = evaluate(
-                [agent, None],
+            determ_avg_rewards = evaluate(
+                list(agents.values()),
                 obs_rms_dict,
                 args.env_name,
                 args.num_processes,
@@ -182,28 +179,24 @@ def main():
                 connections_2=connections_2,
             )
 
-            determ_avg_reward = results["robot_1"]
+            for a in vec_env.agents:
+                if determ_avg_rewards[a] > max_determ_avg_rewards[a]:
+                    max_determ_avg_rewards[a] = determ_avg_rewards[a]
+                    controller_path = os.path.join(log_dirs[a], "controller.pt")
+                    torch.save(
+                        [
+                            agents[a].state_dict(),
+                            obs_rms_dict[a],
+                        ],
+                        controller_path,
+                    )
 
-            print(f"Evaluated using {args.num_evals} episodes. Mean reward: {determ_avg_reward}\n")
-
-            if determ_avg_reward > max_determ_avg_reward:
-                max_determ_avg_reward = determ_avg_reward
-                controller_path = os.path.join(args.save_path, "agent1_controller.pt")
-                print(f"Saving {controller_path} with avg reward {max_determ_avg_reward}\n")
-                torch.save(
-                    [
-                        agent.state_dict(),
-                        obs_rms_dict["robot_1"],
-                    ],
-                    controller_path,
-                )
-
-            with open(eval_csv_path, "a") as f:
-                writer = csv.writer(f)
-                writer.writerow([j, total_num_steps, determ_avg_reward, max_determ_avg_reward])
+                with open(eval_csv_paths[a], "a") as f:
+                    writer = csv.writer(f)
+                    writer.writerow([j, total_num_steps, determ_avg_rewards[a]])
 
     results = evaluate(
-        [agent, None],
+        list(agents.values()),
         vec_env.obs_rms_dict,
         args.env_name,
         args.num_processes,

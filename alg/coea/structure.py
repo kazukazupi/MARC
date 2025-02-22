5afa4219769920094ec3640fd1f7794728954721
kazukazupi <22kazuaki22@gmail.com>
2025-02-22 18:03:07 +0900

    implemented Structure.from_save_path

--- alg/coea/structure.py
+++ alg/coea/structure.py
@@ -6,17 +6,24 @@ import numpy as np
 
 class Structure:
 
-    def __init__(self, save_path: str, body: np.ndarray, connections: np.ndarray):
+    def __init__(self, save_path: str, body: np.ndarray, connections: np.ndarray, save: bool = True):
 
         self.save_path = save_path
         self.body = body
         self.connections = connections
 
-        os.mkdir(self.save_path)
-        np.save(os.path.join(self.save_path, "body.npy"), body)
-        np.save(os.path.join(self.save_path, "connections.npy"), connections)
+        if save:
+            os.mkdir(self.save_path)
+            np.save(os.path.join(self.save_path, "body.npy"), body)
+            np.save(os.path.join(self.save_path, "connections.npy"), connections)
 
     def get_latest_controller_path(self) -> str:
         controller_paths = sorted(glob.glob(os.path.join(self.save_path, "controller_*.pt")))
         assert controller_paths, f"Controller for {self.save_path} is not found."
         return max(controller_paths, key=os.path.getctime)
+
+    @classmethod
+    def from_save_path(cls, save_path: str) -> "Structure":
+        body = np.load(os.path.join(save_path, "body.npy"))
+        connections = np.load(os.path.join(save_path, "connections.npy"))
+        return cls(save_path, body, connections, save=False)

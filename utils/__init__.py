140bd5c1c28a90645888c39cb19828ca3632f68a
kazukazupi <22kazuaki22@gmail.com>
2025-09-27 19:25:46 +0900

    環境内のエージェント名をrobot_1, robot_2で固定

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
+from utils.agent_id import AGENT_1, AGENT_2, AGENT_IDS, AgentID, get_opponent_id  # noqa: F401
 from utils.arguments import get_args, load_args, save_args  # noqa: F401
-from utils.name import get_agent_names  # noqa: F401
 from utils.seed import set_seed  # noqa: F401

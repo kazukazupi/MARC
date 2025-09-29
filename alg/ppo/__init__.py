373b49a20cf3f9452c48e086b91802fdfc80be7c
kazukazupi <22kazuaki22@gmail.com>
2025-09-30 00:02:13 +0900

    analysis/visualizeをppo単体実験にも対応

--- alg/ppo/__init__.py
+++ alg/ppo/__init__.py
@@ -3,6 +3,5 @@ from alg.ppo.evaluate import evaluate  # noqa: F401
 from alg.ppo.model import Agent  # noqa: F401
 from alg.ppo.ppo import PPO  # noqa: F401
 from alg.ppo.ppo_utils import update_linear_schedule  # noqa: F401
-from alg.ppo.single_agent_envs import make_vec_envs as make_vec_saenvs  # noqa: F401
 from alg.ppo.storage import RolloutStorage  # noqa: F401
 from alg.ppo.train import train, train_against_fixed_opponent  # noqa: F401

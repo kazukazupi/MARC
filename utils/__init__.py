d1d64872c499ae2584fd9e927e463cfffa6da558
kazukazupi <22kazuaki22@gmail.com>
2025-09-30 11:36:20 +0900

    ダミーロボットの構造を構築

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from utils.agent_id import AGENT_1, AGENT_2, AGENT_IDS, AgentID, get_opponent_id  # noqa: F401
 from utils.arguments import get_args, load_args, save_args  # noqa: F401
+from utils.dummy_robot import get_dummy_robot  # noqa: F401
 from utils.seed import set_seed  # noqa: F401
